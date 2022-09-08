4c6ef30230ff09529be1f46db35bff2158743f04
CassyBlakes <destinyezinokafor@gmail.com>
2022-09-08 16:45:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,14 @@
 /**
  * main - print the exact string
  *
- * Description: print "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * Description: print exactly the string 
+ * "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  *
  * Return: 0
 */
 
 int main(void)
 {
-	write(3, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 29);
+	write(3, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 29);
 	return (1);
 }
