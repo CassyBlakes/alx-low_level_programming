afedbd22b9d18e27ca63ed62aa149be1eff5b4d3
CassyBlakes <destinyezinokafor@gmail.com>
2022-09-10 13:17:08 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,22 +14,21 @@ int main(void)
 
 	for (i = 0, i <= 9, i++)
 	{
-		if (j > i)
+		for (j = 0, j <= 9, j++)
 		{
-
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 8)
-
+			if (j > i)
 			{
-				putchar(',');
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8)
+				{
+					putchar(',');
 
-				putchar(' ');
-
+					putchar(' ');
+				}
 			}
 		}
 	}
-}
 	putchar('\n');
 	return (0);
 }
