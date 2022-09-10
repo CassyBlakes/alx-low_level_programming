b9b28f31a1e9f9c010aa91a93e0dbcfe25cbce97
CassyBlakes <destinyezinokafor@gmail.com>
2022-09-09 17:29:33 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <sstdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+
+}
