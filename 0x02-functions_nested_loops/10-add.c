6c4d6ade1d863b13b23fc2adab9e0e2b028c4f12
CassyBlakes <destinyezinokafor@gmail.com>
2022-09-13 17:32:49 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *add - adds two integers
+ * and returns the result
+ *@a: first operand
+ *@b: second operand
+ *
+ *Return: return the su. of the two operand
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+
+}
+
