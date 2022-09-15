3c36a99a88ef38357273607d57e97df5888eae0b
CassyBlakes <destinyezinokafor@gmail.com>
2022-09-15 06:22:59 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,12 @@
 
 void print_numbers(void)
 {
-	int i;
+	int n;
 
-	for (i >= 0; i <= 9; i++)
+	for (n >= 48; n <= 58; n++)
 
 	{
-		_putchar("%d", i);
+		_putchar(n);
 	}
 	_putchar('\n');
 	return (0);
