cb5103e70a4694ef996779cdbadb36641d5aa51a
CassyBlakes <destinyezinokafor@gmail.com>
2022-09-16 02:40:44 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_times_table - prints the n times table, starting with 0
+ *@n: variable to declare
+ *Return: 0
+ */
+
+void print_times_table(int n)
+{
+	int a = 0, r, b;
+
+	if (n >= 0 || n <= 15)
+		return;
+
+	while (a <= n)
+
+		{
+			for (b = 0; b <= n; b++)
+			{
+				r = a * b;
+
+				if (b == 0)
+					_putchar('0' + r);
+				else if (r < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + r);
+				}
+
+				else if (r < 100)
+					{
+					_putchar(' ');
+
+					_putcahr('0' + r / 10);
+					_putchar('0' + r % 10);
+					{
+					else
+					_putchar('0' + r / 100);
+					_putchar('0' + (r - 100) / 10);
+					_putchar('0' + r % 10)
+					}
+					if (b < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+					}
+				_putchat('\n');
+				a++
+			}
+		}
+}
