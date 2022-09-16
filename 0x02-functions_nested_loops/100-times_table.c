ddf8cf5883f7109527e868a2b7a29d9ffa680413
CassyBlakes <destinyezinokafor@gmail.com>
2022-09-16 02:46:03 -0700

    101-natural.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,54 +2,49 @@
 #include <stdio.h>
 
 /**
- *print_times_table - prints the n times table, starting with 0
- *@n: variable to declare
- *Return: 0
+ * print_times_table - Function to print the mutiplication table
+ * @n: Variable to declare
+ * Return: Always void
  */
-
 void print_times_table(int n)
 {
 	int a = 0, r, b;
 
-	if (n >= 0 || n <= 15)
+	if (n < 0 || n > 15)
 		return;
 
 	while (a <= n)
-
+	{
+		for (b = 0; b <= n; b++)
 		{
-			for (b = 0; b <= n; b++)
+			r = a * b;
+			if (b == 0)
+				_putchar('0' + r);
+			else if (r < 10)
 			{
-				r = a * b;
-
-				if (b == 0)
-					_putchar('0' + r);
-				else if (r < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + r);
-				}
-
-				else if (r < 100)
-					{
-					_putchar(' ');
-
-					_putcahr('0' + r / 10);
-					_putchar('0' + r % 10);
-					{
-					else
-					_putchar('0' + r / 100);
-					_putchar('0' + (r - 100) / 10);
-					_putchar('0' + r % 10)
-					}
-					if (b < n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-					}
-				_putchat('\n');
-				a++
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + r);
+			}
+			else if (r < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + r / 10);
+				_putchar('0' + r % 10);
+			}
+			else
+			{
+				_putchar('0' + r / 100);
+				_putchar('0' + (r - 100) / 10);
+				_putchar('0' + r % 10);
+			}
+			if (b < n)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
+		_putchar('\n');
+		a++;
+	}
 }
