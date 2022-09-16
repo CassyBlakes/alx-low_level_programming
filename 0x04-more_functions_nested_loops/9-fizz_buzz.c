e9727064d88cc16bf8fe68f6a06cc23d384cbf06
CassyBlakes <destinyezinokafor@gmail.com>
2022-09-16 03:25:50 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,29 +13,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 != 0))
-		{
-			printf("Fizz");
-		}
-		else if ((i % 5 == 0) && (i % 3 != 0))
-		{
-			printf("Buzz");
-		}
-		else if ((i % 3 == 0)  && (i % 5 == 0))
-		{
-			printf("FizzBuzz");
-		}
-		else if (i == 1)
-		{
-			printf("%d", i);
-		}
+		if (i % 3 == 0 && i % 5 == 0)
+
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
 		else
-		{
-			printf("%d", i);
-		}
+			printf("%d ", i);
 	}
-	printf("\n");
-	return (0);
+	printf("Buzz\n");
 }
