5b158823b21b8b2f02512aa3647bba1ea7e796b4
CassyBlakes <destinyezinokafor@gmail.com>
2022-09-22 11:55:34 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,16 +9,15 @@ void print_number(int n)
 {
 	unsigned int n1;
 
+	n1 = n;
+
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('_');
+		n1 = -n;
 	}
-	else
-	{
-		n1 = n;
-	}
-	if (n1 / 10)
+
+	if (n1 / 10 != 0)
 		print_number(n1 / 10);
 	_putchar((n1 % 10) + '0');
 }
